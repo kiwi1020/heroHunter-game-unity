3e548bd7252092d53e593370d4100a60fc99ac4e
TTobing3 <100311186+TTobing3@users.noreply.github.com>
2023-09-25 11:37:16 +0900

    푸쉬 테스트
    1. testScript 디버그 추가
    2. 이미지 으브젝트 배치

--- Assets/TestScript_00.cs
+++ Assets/TestScript_00.cs
@@ -7,7 +7,7 @@ public class TestScript_00 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("진짜 괴영잡 대박날 듯?");
     }
 
     // Update is called once per frame
