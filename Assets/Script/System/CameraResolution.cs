5ee1887e709ad4a3c24e893faf0dae915760657d
TTobing3 <100311186+TTobing3@users.noreply.github.com>
2023-10-14 15:53:38 +0900

    장태규 ) 파일 정리 및 해상도 설정
    -고정 해상도 카메라 및 캔버스 설정
    -파일 정리

---
+++ Assets/Script/System/CameraResolution.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResolution : MonoBehaviour
+{
+    //해상도 고정해주는 스크립트
+
+    void Start()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleHeight = ((float)Screen.width / Screen.height / ((float)16.0f / 9.0f));
+        float scaleWidth = 1f / scaleHeight;
+        if (scaleHeight < 1)
+        {
+            rect.height = scaleHeight;
+            rect.y = (1f - scaleHeight) / 2f;
+        }
+        else
+        {
+            rect.width = scaleWidth;
+            rect.x = (1f - scaleWidth) / 2f;
+        }
+        camera.rect = rect;
+
+        Application.targetFrameRate = 45;
+    }
+
+    void Update()
+    {
+        GL.Clear(true, true, Color.black);
+    }
+    void OnPreCull() => GL.Clear(true, true, Color.black);
+}
