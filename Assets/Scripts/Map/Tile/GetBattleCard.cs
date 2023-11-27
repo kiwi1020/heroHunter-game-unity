4d408aea27af754a5f431b29c132940b1232c99a
Kimsungwook <kkds03264@gmail.com>
2023-11-27 17:39:44 +0900

    김성욱) 타일 이벤트 추가

--- Assets/Scripts/Map/Tile/GetBattleCard.cs
+++ Assets/Scripts/Map/Tile/GetBattleCard.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetBattleCard : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI cardNameText, cardDesText;
-
+    [SerializeField] Image illust;
     BattleCardData battleCardData;
     // Start is called before the first frame update
     public void SetCard(BattleCardData _battleCardData)
@@ -15,5 +16,7 @@ public class GetBattleCard : MonoBehaviour
         cardNameText.text = _battleCardData.name;
         cardNameText.color = Color.white;
         cardDesText.text = _battleCardData.skillData.effects[0];
+
+        illust.sprite = DataManager.instance.AlllBattleCardIllusts.Find(x => x.name == battleCardData.name).sprite;
     }
 }
--- Assets/Scripts/Map/Tile/MapTile.cs
+++ Assets/Scripts/Map/Tile/MapTile.cs
@@ -11,9 +11,7 @@ public class MapTile : MonoBehaviour
 
     public TextMeshPro tileName; //타일이름 Text
     public Button startButton; //배틀시작 버튼
-
-    public List<GameObject> GetBattleCard;
-
+  
     //타일이름 설정, 이름은 정한 후 수정
     public void SetTile(TileData _tileData)
     {
@@ -43,14 +41,12 @@ public class MapTile : MonoBehaviour
 
             case "함정":
                 MapSystem.instance.gainEvent.SetEvent(this);
+                DeleteBattleCard();
                 break;
 
             case "보상":
-                MapSystem.instance.gainEvent.SetEvent(this);                
-                foreach(GameObject i in GetBattleCard)
-                {
-                    i.SetActive(true);
-                }
+                MapSystem.instance.gainEvent.SetEvent(this);
+                GetBattleCard();
                 break;
 
             default:               
@@ -67,5 +63,40 @@ public class MapTile : MonoBehaviour
         //startButton.gameObject.SetActive(false);
 
         SceneManager.LoadScene("PlayScene"); // 임시로 바로 시작
+    }
+    public void GetBattleCard()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject cardObject = GameObject.Find("BattleCard").transform.GetChild(i).gameObject;
+            print(cardObject.name);
+
+            string[] BattleCardNames = new string[] { "갈라치기", "급소 찌르기", "뒤통수치기", "뺨때리기", "성급한판단", "속사", "비열한 찌르기" };
+
+            // 랜덤으로 BattleCardNames에서 카드 이름을 선택
+            string randomCardName = BattleCardNames[Random.Range(0, BattleCardNames.Length)];
+
+            // 선택한 카드 이름으로 실제 데이터를 얻어옴
+            BattleCardData card = DataManager.instance.AllBattleCardDatas[randomCardName];
+            print(card.name);
+
+            // GetBattleCard 컴포넌트를 얻어와서 카드를 설정
+            var battleCard = cardObject.GetComponent<GetBattleCard>();
+            battleCard.SetCard(card);
+
+            PlayerData.playerBattleCardDeck.Add(card);
+        }
+    }
+    public void DeleteBattleCard()
+    {   
+        if (PlayerData.playerBattleCardDeck.Count != 0)
+        {
+            PlayerData.playerBattleCardDeck.RemoveAt(
+                Random.Range(0, PlayerData.playerBattleCardDeck.Count));
+        }
+    }
+    public void SelectEvent()
+    {
+
     }
 }
