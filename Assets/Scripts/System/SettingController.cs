82612f78cf2ecacff39915958442abf93c9bc9c6
kiwi1020 <108050749+kiwi1020@users.noreply.github.com>
2023-12-03 13:00:28 +0900

    오디오 설정 창

---
+++ Assets/Scripts/System/SettingController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingController : MonoBehaviour
+{
+    public GameObject manager;
+    public AudioMixer masterMixer;
+    public Slider[] sliders;
+    public GameObject AudioManager;
+
+    public void ControllVolume(string audioType)
+    {
+        switch (audioType)
+        {
+            case "Bgm":
+                masterMixer.SetFloat("Bgm", sliders[0].value);
+                break;
+
+            case "Sfx":
+                masterMixer.SetFloat("Sfx", sliders[1].value);
+                break;
+        }
+      
+    }
+
+    public void EnableSettingUI()
+    {
+        AudioManager = GameObject.Find("AudioManager");
+        AudioManager.GetComponent<SoundManager>().UISfxPlay(0);
+        manager.SetActive(true);
+    }
+
+
+    public void DisableSettingUI()
+    {
+        AudioManager = GameObject.Find("AudioManager");
+        AudioManager.GetComponent<SoundManager>().UISfxPlay(0);
+        GameObject.Find("SettingUI").SetActive(false);
+    }
+
+    public void ActEndButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
