2e93c5f89226ae65ca82dc04485b6281ba1762ac
Kimsungwook <kkds03264@gmail.com>
2023-12-10 21:43:30 +0900

    김성욱)수정

--- Assets/Scripts/Map/MapSystem.cs
+++ Assets/Scripts/Map/MapSystem.cs
@@ -128,10 +128,9 @@ public class MapSystem : MonoBehaviour
     void setupMap()
     {
         moveCardDraw = true;
-        GenerateTileObjects(20);
+        GenerateTileObjects(6);
         SetTileMapData();
         MoveCameraToTargetTile(tileMap[curTileNum]);
-
         player = Instantiate(playerPrefab, tileMap[curTileNum].transform.position, tileMap[curTileNum].transform.rotation); 
         if (!player.activeSelf)
         {
